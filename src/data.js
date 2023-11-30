0ac8d4e6c896d9c9e6bcbe7d5c5e390a8217b55f
Canaga81 <Canagamemmedov@gmail.com>
2023-11-30 15:42:11 +0400

    Data name

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+//! Navbar Link Data
+
 export const links = [
     {
         name: "Home",
@@ -27,7 +29,7 @@ export const links = [
 
 
 
-
+//! Programs Component Data
 
 
 
@@ -60,8 +62,7 @@ export const programs = [
 
 
 
-
-
+//! Values Component Data
 
 
 
@@ -90,8 +91,7 @@ export const values = [
 
 
 
-
-
+//! FAQs Component Data
 
 
 
@@ -132,7 +132,7 @@ export const faqs = [
 
 
 
-
+//! Testimonial Page Data
 
 
 
@@ -177,7 +177,7 @@ export const testimonials = [
 
 
 
-
+//! Plans Page Data
 
 
 export const plans = [
@@ -249,58 +249,58 @@ export const plans = [
 
 
 
+//! Trainers Page Data
 
 
+import Trainer1 from './images/trainer1.jpg';
+import Trainer2 from './images/trainer2.jpg';
+import Trainer3 from './images/trainer3.jpg';
+import Trainer4 from './images/trainer4.jpg';
+import Trainer5 from './images/trainer5.jpg';
+import Trainer6 from './images/trainer6.jpg';
 
-// const Trainer1 = require( './images/trainer1.jpg' );
-// const Trainer2 = require( './images/trainer2.jpg' );
-// const Trainer3 = require( './images/trainer3.jpg' );
-// const Trainer4 = require( './images/trainer4.jpg' );
-// const Trainer5 = require( './images/trainer5.jpg' );
-// const Trainer6 = require( './images/trainer6.jpg' );
 
-
-// export const trainers = [
-//     {
-//         id: 1,
-//         image: Trainer1,
-//         name: 'John Doe',
-//         job: 'Aerobic Trainer',
-//         socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
-//     },
-//     {
-//         id: 2,
-//         image: Trainer2,
-//         name: 'Daniel vinyo',
-//         job: 'Speed Trainer',
-//         socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
-//     },
-//     {
-//         id: 3,
-//         image: Trainer3,
-//         name: 'Edem Quist',
-//         job: 'Flexibility Trainer',
-//         socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
-//     },
-//     {
-//         id: 4,
-//         image: Trainer4,
-//         name: 'Shatta Wale',
-//         job: 'Body Composition Trainer',
-//         socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
-//     },
-//     {
-//         id: 5,
-//         image: Trainer5,
-//         name: 'Diana Ayi',
-//         job: 'Circuit Trainer',
-//         socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
-//     },
-//     {
-//         id: 6,
-//         image: Trainer6,
-//         name: 'Wayne Carter',
-//         job: 'Physical Intelligence Trainer',
-//         socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
-//     }
-// ];
+export const trainers = [
+    {
+        id: 1,
+        image: Trainer1,
+        name: 'John Doe',
+        job: 'Aerobic Trainer',
+        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+    },
+    {
+        id: 2,
+        image: Trainer2,
+        name: 'Daniel vinyo',
+        job: 'Speed Trainer',
+        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+    },
+    {
+        id: 3,
+        image: Trainer3,
+        name: 'Edem Quist',
+        job: 'Flexibility Trainer',
+        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+    },
+    {
+        id: 4,
+        image: Trainer4,
+        name: 'Shatta Wale',
+        job: 'Body Composition Trainer',
+        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+    },
+    {
+        id: 5,
+        image: Trainer5,
+        name: 'Diana Ayi',
+        job: 'Circuit Trainer',
+        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+    },
+    {
+        id: 6,
+        image: Trainer6,
+        name: 'Wayne Carter',
+        job: 'Physical Intelligence Trainer',
+        socials: ['https://instagram.com/', 'https://twitter.com/', 'https://facebook.com/', 'https://linkedin.com/']
+    }
+];
